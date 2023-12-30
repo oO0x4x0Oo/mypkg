4d7557be7117c69b93e41d240ced9a28ed7118cc
Toshito Kimura <a4392966@gmail.com>
2023-12-30 19:26:09 +0900

    Fix test

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -9,7 +9,6 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-<<<<<<< HEAD
      talker = launch_ros.actions.Node(
          package='mypkg',
          executable='talker',
@@ -21,16 +20,3 @@ def generate_launch_description():
          )
 
      return launch.LaunchDescription([talker, listener])
-=======
-    talker = launch_ros.actions.Node(
-        package='mypkg',
-        executable='talker',
-        )
-    listener = launch_ros.actions.Node(
-        package='mypkg',
-        executable='listener',
-        output='screen'
-        )
-
-    return launch.LaunchDescription([talker, listener])
->>>>>>> lesson10
