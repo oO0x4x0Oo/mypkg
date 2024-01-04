cc431d8075a0e194c4c8e72fa85af8bc06259bc1
Toshito Kimura <a4392966@gmail.com>
2024-01-04 15:49:17 +0900

    Fix test

--- test/test.bash
+++ test/test.bash
@@ -10,4 +10,7 @@ colcon build
 
 source $dir/.bashrc
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'age: 20'
