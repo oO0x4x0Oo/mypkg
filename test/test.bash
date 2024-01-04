9467bb919bfd3e5b3b3bf858b278f623a0efbe83
Toshito Kimura <a4392966@gmail.com>
2024-01-04 16:10:50 +0900

    Add test

--- test/test.bash
+++ test/test.bash
@@ -10,7 +10,22 @@ colcon build
 
 source $dir/.bashrc
 
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 5 ros2 launch mypkg talk_listen.launch.py &> /tmp/mypkg.log &
 
-cat /tmp/mypkg.log  |
-grep 'age: 20'
+sleep 2
+
+python3 $dir/ros2_ws/src/mypkg/mypkg/listener.py
+
+listener_exit_code=$?
+
+ros2 node kill /talker
+ros2 node kill /listener
+
+cat /tmp/mypkg.log
+
+if [ $listener_exit_code -eq 0 ]; then
+  echo "テスト成功：ログファイルに 'age: 20' が見つかりました。"
+else
+  echo "テスト失敗：ログファイルに 'age: 20' が見つかりませんでした。"
+fi
+
