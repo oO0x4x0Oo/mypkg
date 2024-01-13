748582951b4e4bc41863dfd352ffa693474ef1d6
oO0x4x0Oo <a4392966@gmail.com>
2024-01-13 23:23:25 +0900

    Fix test

--- test/test.bash
+++ test/test.bash
@@ -6,12 +6,23 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws
-colcon build
-
 source $dir/.bashrc
 
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cd $dir/ros2_ws/src
+
+git clone https://github.com/oO0x4x0Oo/person_msgs.git
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+
+ros2 interface show "person_msgs/srv/Query"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+
+timeout 2 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 ng () {
     echo "NG at Line $1"
@@ -23,8 +34,7 @@ res=0
 cat /tmp/mypkg.log |
 grep ": 20"
 
-[ "$?" = 0 ] || ng $LINENO    
+[ "$?" = 0 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo "OK"
 exit $res
-
